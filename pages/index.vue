cabc5e129904f21bc0cb7a4baa3fa4a1d24769f5
hino <hino@freedomland.jp>
2020-08-27 14:24:31 +0900

    【Nuxt.js】vuexの使用

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="">
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from '~/components/counter.vue'
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+
+</style>
